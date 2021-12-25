7ac1e145546d48ccb185aecf872bf863f6e1fa9a
v_nikishin <sprtes@gmail.com>
2021-12-25 21:12:35 +0300

    Task 6 to review

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-const {src, dest, watch, series, parallel, task, on} = require('gulp');
+const {src, dest, watch, series, parallel, task} = require('gulp');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
 const sassGlob = require('gulp-sass-glob');
 const autoprefixer = require('gulp-autoprefixer');
-const px2rem = require('gulp-smile-px2rem');
 const gcmq = require('gulp-group-css-media-queries');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
@@ -14,19 +13,20 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const gulpif = require('gulp-if');
+const sassLint = require('gulp-sass-lint');
 
 const env = process.env.NODE_ENV;
 
-const {SRC_PATH, DIST_PATH, JS_LIBS, STYLES_LIBS} = require('./gulp.config')
+const {SRC_PATH, DIST_PATH, JS_LIBS, STYLES_LIBS, VENDOR_LIBS} = require('./gulp.config')
 
 task('copyImg', () => {
   return src(`${SRC_PATH}/images/*.*`)
-    .pipe(dest(`${DIST_PATH}/images/`))
+    .pipe(dest(`${DIST_PATH}/images/`));
 });
 
 task('copyVendors', () => {
   return src([`${SRC_PATH}/js/vendors/**/*.*`, `!${SRC_PATH}/js/vendors/slick/slick.js`])
-    .pipe(dest(`${DIST_PATH}/js/vendors/`))
+    .pipe(dest(`${DIST_PATH}/js/vendors/`));
 });
 
 task('delImg', () => {
@@ -54,23 +54,37 @@ task('reload', (done) => {
 });
 
 task('compileScss', () => {
-  return src([...STYLES_LIBS,
-    "src/js/vendors/slick/slick.css",
-    "src/js/vendors/slick/slick-theme.css",])
+  return src([...VENDOR_LIBS,
+    ...STYLES_LIBS,])
+    .pipe(sassLint({
+      files: {ignore: [...VENDOR_LIBS]},
+      configFile: '.sass-lint.yml'
+    }))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
     .pipe(gulpif(env === 'dev', sourcemaps.init()))
     .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulpif(env === 'prod', px2rem()))
     .pipe(gulpif(env === 'prod', autoprefixer()))
     .pipe(gulpif(env === 'prod', gcmq()))
     .pipe(gulpif(env === 'prod', cleanCSS({debug: true}, (details) => {
       console.log(`${details.name}: ${details.stats.originalSize}`);
       console.log(`${details.name}: ${details.stats.minifiedSize}`);
     })))
+    .pipe(gulpif(env === 'prod', rename('main.min.css')))
     .pipe(gulpif(env === 'dev', sourcemaps.write()))
     .pipe(dest(`./${DIST_PATH}/styles/`))
     .pipe(browserSync.stream());
 });
+
+task('lint-check', () => {
+  return src(`${SRC_PATH}/styles/**/*.scss`)
+  .pipe(sassLint({
+    configFile: '.sass-lint.yml'
+  }))
+  .pipe(sassLint.format())
+  .pipe(sassLint.failOnError())
+})
 
 task('compilePug', () => {
   return src(`${SRC_PATH}/pug/pages/*.pug`)
@@ -104,7 +118,7 @@ task('watchers', (done) => {
   done();
 });
 
-task("build", series('clean', 'copyImg', 'copyVendors', parallel('compilePug', 'scripts', 'compileScss')));
+task("build", series('clean', 'copyImg', 'copyVendors', parallel('compilePug', 'scripts', 'compileScss'), 'lint-check'));
 task("serve", series('build', parallel('server', 'watchers')));
 
 
--- src/js/module-two.js
+++ src/js/module-two.js
@@ -1,7 +1,7 @@
 ;(function() {
-    const TIMEOUT = 4000;
-    setTimeout(()=> {
-    console.log('Hello from module two')
+  const TIMEOUT = 4000;
+  setTimeout(()=> {
+  console.log('Hello from module two')
 },TIMEOUT);
 })();
 
